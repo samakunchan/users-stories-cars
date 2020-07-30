191b0fe0014a3278235ecdf17852b399c78f5e38
Samakunchan <samakunchan@gmail.com>
2020-07-30 11:58:41 +0200

    feat: style of app
    - fix: remove .css config to .scss
    - Added a short config style for toolbar
    - Added shared component for the app: header and footer
    - Added material module for header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatListModule, MatButtonModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
